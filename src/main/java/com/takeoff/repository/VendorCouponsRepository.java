3cb257d60aa20fe51a5e46ce01c40619b7f43756
pavan6717624 <84194405+pavan6717624@users.noreply.github.com>
2021-10-01 17:43:45 +0530

    depoly

--- src/main/java/com/takeoff/repository/VendorCouponsRepository.java
+++ src/main/java/com/takeoff/repository/VendorCouponsRepository.java
@@ -76,6 +76,9 @@ public interface VendorCouponsRepository  extends PagingAndSortingRepository<Ven
 			+ "(:userId) = 0) and (c.image.subCateogry.category.id=(:category) or (:category=0)) and "
 			+" (c.image.subCateogry.id=(:subCategory) or (:subCategory=0))  and "
 			
+	       		+ " ((c.couponType.id > 1) or (c.couponType.id = 1 and c.vendor.user.userId not in (select r.vendor.userId from Redemption r where r.userRedempted = true and r.vendorAccepted = true and r.customer.userId=(:customerId)))) and "
+			
+	       
 			+ "( c.image.keywords like :keyword1 or c.image.keywords like :keyword2 or c.image.keywords like :keyword3 or "
 			+ " c.image.keywords like :keyword4 or c.image.keywords like :keyword5 or "
 			
