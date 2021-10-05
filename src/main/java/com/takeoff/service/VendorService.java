f033c3b8bc7db3db1b5ed84cd6ef37e934593cfd
pavan6717624 <84194405+pavan6717624@users.noreply.github.com>
2021-10-05 15:52:07 +0530

    depoly

--- src/main/java/com/takeoff/service/VendorService.java
+++ src/main/java/com/takeoff/service/VendorService.java
@@ -6,6 +6,7 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.security.SecureRandom;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
