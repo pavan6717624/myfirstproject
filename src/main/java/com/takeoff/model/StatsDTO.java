b71e8c9a13cecc0c65347865dfa11b7fba6fde95
pavan6717624 <84194405+pavan6717624@users.noreply.github.com>
2021-10-07 18:15:11 +0530

    depoly

---
+++ src/main/java/com/takeoff/model/StatsDTO.java
@@ -0,0 +1,15 @@
+package com.takeoff.model;
+
+public interface StatsDTO {
+	
+	public String getRoleName();
+	
+	public Long getTodayCount();
+	
+	public Long getMonthCount();
+	
+	public Long getTotalCount();
+	
+	
+
+}
