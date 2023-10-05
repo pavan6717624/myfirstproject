a51c0b16873fca6671d18b0c694ebb6d1224841f
pavan6717624 <84194405+pavan6717624@users.noreply.github.com>
2023-10-05 11:51:21 +0530

    Update FnoStockRepository.java

--- src/main/java/com/takeoff/repository/FnoStockRepository.java
+++ src/main/java/com/takeoff/repository/FnoStockRepository.java
@@ -3,7 +3,7 @@ package com.takeoff.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.mytradingsetup.domain.FnoStock;
+import com.takeoff.domain.FnoStock;
 
 
 @Repository
