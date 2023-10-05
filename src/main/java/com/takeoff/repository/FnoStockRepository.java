bf49811bdde0e41e9ebf083633def3afd4e4caae
pavan6717624 <84194405+pavan6717624@users.noreply.github.com>
2023-10-05 11:49:21 +0530

    Add files via upload

--- src/main/java/com/takeoff/repository/FnoStockRepository.java
+++ src/main/java/com/takeoff/repository/FnoStockRepository.java
@@ -1,9 +1,9 @@
-package com.takeoff.repository;
+package com.mytradingsetup.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.takeoff.domain.FnoStock;
+import com.mytradingsetup.domain.FnoStock;
 
 
 @Repository
