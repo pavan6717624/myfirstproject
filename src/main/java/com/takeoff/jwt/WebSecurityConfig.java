7921ac12f04f41e50793f5862b6c92dbd5037ad6
pavan6717624 <84194405+pavan6717624@users.noreply.github.com>
2024-03-21 21:55:23 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/takeoff/jwt/WebSecurityConfig.java
+++ src/main/java/com/takeoff/jwt/WebSecurityConfig.java
@@ -61,7 +61,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.authorizeRequests()
 				.antMatchers("/login").permitAll()
 				.antMatchers("/GMAPI/**").permitAll()
-				.antMatchers("/Heidigi/**").permitAll()
+				.antMatchers("/check").permitAll()
 				.antMatchers("/MYTRADE/**").permitAll()
 				.antMatchers("/approveSMS").permitAll()
 				.antMatchers("/getOrderId").permitAll()
